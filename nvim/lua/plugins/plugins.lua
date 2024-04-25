e435654096511dae4c1d85f4120b3edfbf5fd075
kringla <krngle@gmail.com>
2024-04-25 09:09:18 +0200

    Added TODO.md and diabled mouse

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -19,4 +19,23 @@ return {
 			["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
 		},
 	},
+	{
+		"cameron-wags/rainbow_csv.nvim",
+		config = true,
+		ft = {
+			"csv",
+			"tsv",
+			"csv_semicolon",
+			"csv_whitespace",
+			"csv_pipe",
+			"rfc_csv",
+			"rfc_semicolon",
+		},
+		cmd = {
+			"RainbowDelim",
+			"RainbowDelimSimple",
+			"RainbowDelimQuoted",
+			"RainbowMultiDelim",
+		},
+	},
 }
